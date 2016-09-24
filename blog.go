424de97867a49070abc3b49e30a0c0275ada581b
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-09-25 01:26:26 +0200

    added sqlite support

--- blog.go
+++ blog.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 	"html/template"
 	"regexp"
+	"time"
+	"log"
+	"fmt"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var templates = template.Must(template.ParseFiles("./tmpl/index.html", "./tmpl/edit.html", "./tmpl/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+const PORT = 8080
+var DATABASE *sql.DB
 
 type Page struct {
 	Title string
@@ -88,17 +95,48 @@ func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.Hand
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "index.html", nil)
+	params := struct {
+		Name string
+		Year int
+	} { "Blog system", time.Now().Year() }
+
+	err := templates.ExecuteTemplate(w, "index.html", params)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+
+func db() {
+	var err error
+	DATABASE, err = sql.Open("sqlite3", "./blog.db")
+
+	if err != nil {
+		log.Fatal("No database")
+	}
+
+	stmt, err := DATABASE.Prepare("INSERT INTO pages(title, content) VALUES(?,?)")
+
+	if err != nil {
+		log.Fatal("Insert issue")
+	}
+
+	_, err = stmt.Exec("test", "cont")
+}
+
 func main() {
+	db()
+
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
-	http.ListenAndServe(":8080", nil)
+
+	port := fmt.Sprintf(":%d", PORT)
+	err := http.ListenAndServe(port, nil)
+
+	if err != nil {
+		log.Fatal("Filed starting server: ", err)
+	}
 }
